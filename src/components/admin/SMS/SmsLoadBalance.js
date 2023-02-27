25cb53da2f38508282fab78498b551f5c42023c7
butaraiza <butaraiza30@gmail.com>
2023-02-27 11:56:19 +0800

    creating dashboard and routes

---
+++ src/components/admin/SMS/SmsLoadBalance.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function SmsLoadBalance() {
+  return (
+    <div>
+      <h1>SMSLoadBalance</h1>
+    </div>
+  )
+}
+
+export default SmsLoadBalance
---
+++ src/components/admin/reports/Expenditures.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Expenditures() {
+  return (
+    <div>
+      <h1>Expenditures</h1>
+    </div>
+  )
+}
+
+export default Expenditures
