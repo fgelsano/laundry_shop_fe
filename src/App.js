ee7282c8fe66e1a3eb6b52fd8743ec0434133ed1
RonnelNemiz <r.nemiz@mlgcl.edu.ph>
2023-03-10 13:02:24 +0800

    create-table-page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Home from './components/frontend/Home';
-import NavbarFe from './layout/frontend/NavbarFe';
 import Register from './components/frontend/auth/Register';
 import Login from './components/frontend/auth/Login';
 import MainLayout from './layout/admin/MainLayout';
@@ -10,31 +9,22 @@ import MainLayout from './layout/admin/MainLayout';
 function App() {
   return (
 
-    // <div className="App">
-    //   <Router>
-
-    //     <Switch>
-
-    //       <Route exact path="/" component={Home}/>
-    //       <Route path="/register" component={Register}/>
-    //       <Route path="/login" component={Login}/>
-    //       <Route path="/admin" name="Admin" render={(props) => <MainLayout {...props} /> } />
-
-    //     </Switch>
-
-    //   </Router>
-      
-    // </div>
+    <div className="App">
       <Router>
 
         <Switch>
-        
-          <Route exact path="/">
-            <Home/>
-          </Route>
- 
-          </Switch>
+
+          <Route exact path="/" component={Home}/>
+          <Route path="/register" component={Register}/>
+          <Route path="/login" component={Login}/>
+          <Route path="/admin" name="Admin" render={(props) => <MainLayout {...props} /> } />
+
+        </Switch>
+
       </Router>
+      
+    </div>
+     
       
     
   );
--- src/components/admin/orders/Order.js
+++ src/components/admin/orders/Order.js
@@ -1,10 +1,189 @@
 import React from 'react'
+import '../../../assets/admin/css/style.css'
 
 function Order() {
   return (
- <div>
-  <h1>Orders</h1>
- </div>
+    <div>
+
+          <div className="container bg-light">
+          <h1>List Of Orders</h1>
+              <div className ="card mt-5">
+                <div className="card-body">
+            
+                <table class="table table-striped">
+    <thead>
+      <tr>
+        <th>Service Type</th>
+        <th>Full Name</th>
+        <th>Address</th>
+        <th>Mobile Number</th>
+        <th>Quantity</th>
+        <th>Weight</th>
+        <th>Total</th>
+        <th>Amount Recieved</th>
+        <th>Change</th>
+        <th>Action</th>
+        <th>Orders</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>Wash Dry&Fold</td>
+        <td>Karen Lina</td>
+        <td>Hilongos,Leyte</td>
+        <td>09252654321</td>
+        <td>T-shrt 15pcs</td>
+        <td>7 kg</td>
+        <td>190</td>
+        <td>200</td>
+        <td>10</td>
+        <td>
+        <select name="cars" id="cars">
+          <option value="volvo">Paid</option>
+          <option value="saab">Unpaid</option>
+          
+        </select>
+        </td>
+        <td>
+                            
+        </td>
+      </tr>
+      <select name="cars" id="cars">
+        <option value="volvo">pending</option>
+          <option value="saab">Inprogress</option>
+          <option value="opel">Completed</option>
+        </select>
+      <tr>
+        <td>Wash Dry&Fold</td>
+        <td>Karen Lina</td>
+        <td>Hilongos,Leyte</td>
+        <td>09252654321</td>
+        <td>T-shrt 15pcs</td>
+        <td>7 kg</td>
+        <td>190</td>
+        <td>200</td>
+        <td>10</td>
+        <td>
+        <select name="cars" id="cars">
+          <option value="volvo">Paid</option>
+          <option value="saab">Unpaid</option>
+          
+        </select>
+        </td>
+        <td>
+        <select name="cars" id="cars">
+        <option value="volvo">pending</option>
+          <option value="saab">Inprogress</option>
+          <option value="opel">Completed</option>
+        </select>
+        </td>
+      </tr>
+      <tr>
+        <td>Wash Dry&Fold</td>
+        <td>Karen Lina</td>
+        <td>Hilongos,Leyte</td>
+        <td>09252654321</td>
+        <td>T-shrt 15pcs</td>
+        <td>7 kg</td>
+        <td>190</td>
+        <td>200</td>
+        <td>10</td>
+        <td>
+        <select name="cars" id="cars">
+          <option value="volvo">Paid</option>
+          <option value="saab">Unpaid</option>
+          
+        </select>
+        </td>
+        <td>
+        <select name="cars" id="cars">
+        <option value="volvo">pending</option>
+          <option value="saab">Inprogress</option>
+          <option value="opel">Completed</option>
+        </select>
+        </td>
+      </tr>
+      <tr>
+        <td>Wash Dry&Fold</td>
+        <td>Karen Lina</td>
+        <td>Hilongos,Leyte</td>
+        <td>09252654321</td>
+        <td>T-shrt 15pcs</td>
+        <td>7 kg</td>
+        <td>190</td>
+        <td>200</td>
+        <td>10</td>
+        <td>
+        <select name="cars" id="cars">
+          <option value="volvo">Paid</option>
+          <option value="saab">Unpaid</option>
+          
+        </select>
+        </td>
+        <td>
+        <select name="cars" id="cars">
+        <option value="volvo">pending</option>
+          <option value="saab">Inprogress</option>
+          <option value="opel">Completed</option>
+        </select>
+        </td>
+      </tr>
+      <tr>
+        <td>Wash Dry&Fold</td>
+        <td>Karen Lina</td>
+        <td>Hilongos,Leyte</td>
+        <td>09252654321</td>
+        <td>T-shrt 15pcs</td>
+        <td>7 kg</td>
+        <td>190</td>
+        <td>200</td>
+        <td>10</td>
+        <td>
+        <select name="cars" id="cars">
+          <option value="volvo">Paid</option>
+          <option value="saab">Unpaid</option>
+          
+        </select>
+        </td>
+        <td>
+        <select name="cars" id="cars">
+        <option value="volvo">pending</option>
+          <option value="saab">Inprogress</option>
+          <option value="opel">Completed</option>
+        </select>
+        </td>
+      </tr>
+      <tr>
+        <td>Wash Dry&Fold</td>
+        <td>Karen Lina</td>
+        <td>Hilongos,Leyte</td>
+        <td>09252654321</td>
+        <td>T-shrt 15pcs</td>
+        <td>7 kg</td>
+        <td>190</td>
+        <td>200</td>
+        <td>10</td>
+        <td>
+        <select name="cars" id="cars">
+          <option value="volvo">Paid</option>
+          <option value="saab">Unpaid</option>
+          
+        </select>
+        </td>
+        <td>
+        <select name="cars" id="cars">
+        <option value="volvo">pending</option>
+          <option value="saab">Inprogress</option>
+          <option value="opel">Completed</option>
+        </select>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+                 </div>
+                </div>
+            </div>
+        </div>
   )
 }
 
