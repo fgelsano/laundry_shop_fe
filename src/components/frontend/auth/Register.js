b466087e07b641399ee7c1fd50277454352595a3
butaraiza <butaraiza30@gmail.com>
2023-03-16 12:36:04 +0800

    create login, regiter and logout

--- src/components/frontend/auth/Register.js
+++ src/components/frontend/auth/Register.js
@@ -1,51 +1,99 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react'
 import NavbarFe from '../../../layout/frontend/NavbarFe';
+import '../../../assets/frontend/style.css';
+import axios from 'axios';
+import swal from 'sweetalert';
+import { useHistory } from 'react-router-dom';
+import bg2 from '../../../assets/frontend/images/bg2.jpg'
+
+
+
+
 
 function Register() {
+  const history = useHistory();
+  const [registerInput, setRegister] = useState({
+    // first_name:'',
+    // last_name:'',
+    // address:'',
+    // landmark:'',
+    // contact_num:'',
+    // email:'',
+    // password:'',
+    name: '',
+    email: '',
+    password: '',
+    error_list: [],
+  });
+
+  const handleInput = (e) => {
+    e.persist();
+    setRegister({ ...registerInput, [e.target.name]: e.target.value });
+  }
+
+  const registerSubmit = (e) => {
+    e.preventDefault();
+
+    const data = {
+      // first_name: registerInput.first_name,
+      // last_name: registerInput.last_name,
+      // address: registerInput.address,
+      // landmark: registerInput.landmark,
+      // contact_num: registerInput.contact_num,
+      // email: registerInput.email,
+      // password: registerInput.password,
+      name: registerInput.name,
+      email: registerInput.email,
+      password: registerInput.password,
+    }
+
+    axios.get('/sanctum/csrf-cookie').then(response => {
+      axios.post(`/api/register`, data).then(res => {
+        if (res.data.status === 200) {
+          localStorage.setItem('auth_token', res.data.token);
+          localStorage.setItem('auth_name', res.data.username);
+          swal("Success", res.data.message, "success");
+          history.push('/');
+        }
+        else {
+          setRegister({ ...registerInput, error_list: res.data.validation_errors });
+        }
+      });
+    });
+  }
   return (
-    <div>
-      <NavbarFe />
-      <div classNameName="container py-5">
-        <div className="row justify-content-center">
-          <div className="col-ml-6">
-            <div className="card">
-              <div className="card-header">
-                <h4>Register</h4>
-              </div>
-              <div className="card-body">
-                <form>
-                  <div className="form-group mb-3">
-                      <label>First Name</label>
-                      <input type="text" class="form-control" value="" required />
-                  </div>
-                  <div className="form-group mb-3">
-                      <label>Last Name</label>
-                      <input type="" class="form-control" value="" required />
-                  </div>
-                  <div className="form-group mb-3">
-                      <label>Address</label>
-                      <input type="" class="form-control" value="" required />
-                  </div>
-                  <div className="form-group mb-3">
-                      <label>Contact</label>
-                      <input type="" class="form-control" value="" required />
-                  </div>
-                  <div className="form-group mb-3">
-                      <label>Email</label>
-                      <input type="" class="form-control" value="" required />
-                  </div>
-                </form>
-              </div>
-
-            </div>
-
-          </div>
-
+    <section>
+          <NavbarFe />
+    <div className='registerLoginbox ' style={{background: `url(${bg2})no-repeat`}}>
+ 
+      <div className='form-box'>
+        <div className='form-value'>
+            <form onSubmit={registerSubmit}>
+                <h2>Register</h2>
+                <div className='inputbox'>
+                  <input type="text" name="name" className='inputInput' onChange={handleInput} value={registerInput.name}  required />
+                  <label className='labelLabel'>Name</label>
+                </div>
+                <div className='inputbox'>                 
+                    <input type="email" name="email" className='inputInput' onChange={handleInput} value={registerInput.email}  required />
+                    <label className='labelLabel'>Email</label>
+                    <span>{registerInput.error_list.email}</span>
+                 </div>
+               <div className='inputbox'>
+                   <input type="text" name="password" className='inputInput' onChange={handleInput} value={registerInput.password}  required />
+                   <label className='labelLabel'>Password</label>         
+                  <span>{registerInput.error_list.password}</span>
+                 </div>
+                 <div >
+                  <button type="submit" name="password" className='buttonButton' >Register</button>
+               </div>
+            </form>
         </div>
 
       </div>
     </div>
+    </section>
+   
   )
 }
 
--- src/layout/frontend/NavbarFe.js
+++ src/layout/frontend/NavbarFe.js
@@ -1,45 +1,82 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
+import React from 'react';
 import logo1 from '../../assets/frontend/images/logo1.png';
-import '../../assets/frontend/style.css'
+import '../../assets/frontend/style.css';
+import { Link, useHistory } from 'react-router-dom';
+import swal from 'sweetalert';
+import axios from 'axios';
+
 
 function NavbarFe() {
+
+    const history = useHistory();
+    const logoutSubmit = (e) => {
+        e.preventDefault();
+
+        axios.post(`/api/logout`).then(res => {
+            if(res.data.status === 200)
+            {
+                localStorage.removeItem('auth_token');
+                localStorage.removeItem('auth_name');
+                swal("Success", res.data.message, "success");
+                history.push('/');
+            }
+        });
+    }
+
+    var AuthButtons = '';
+    if (!localStorage.getItem('auth_token')) {
+        AuthButtons = (
+            <ul className='navbar-nav'>
+                <li className="nav-item">
+                    <Link className="nav-link" to="/register">Register</Link>
+                </li>
+                <li className="nav-item">
+                    <Link className="nav-link" to="/login">Login</Link>
+
+                </li>
+            </ul>
+        );
+    }
+    else {
+        AuthButtons = (
+            <li className="nav-item">
+                <button type="button" onClick={logoutSubmit} className="nav-link btn btn-danger btn-sm text-white" >Logout</button>
+
+            </li>
+        );
+    }
+
     return (
        
-        <nav className="navbar navbar-expand-lg bg-light">
+        <nav className="navbar navbar-expand-lg bg-light sticky-top">
             <div className="logoContainer">
                 <img src={logo1} alt="logo" className="logo-name1" />
             </div>
             <div className="container-fluid">
-                <Link to="/" class="navbar-brand">Laundry Shop Management System</Link>
+                <Link to="/" className="navbar-brand">Laundry Shop Management System</Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
                         <li className="nav-item">
-                            <Link to="/home" class="nav-link active" aria-current="page">Home</Link>
+                            <Link to="/home" className="nav-link active" aria-current="page">Home</Link>
                         </li>
                         <li className="nav-item">
-                            <Link to="/about" class="nav-link active" aria-current="page">About</Link>
+                            <Link to="/about" className="nav-link active" aria-current="page">About</Link>
                         </li>
                         <li className="nav-item">
-                            <Link to="/services" class="nav-link active" aria-current="page">Services</Link>
+                            <Link to="/services" className="nav-link active" aria-current="page">Services</Link>
                         </li>
                         <li className="nav-item">
-                            <Link to="/contact" class="nav-link active" aria-current="page">Contact</Link>
+                            <Link to="/contact" className="nav-link active" aria-current="page">Contact</Link>
                         </li>
                         <li className="nav-item">
-                            <Link to="/order" class="nav-link active" aria-current="page">Order</Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link" to="/login">Login</Link>
-                      
+                            <Link to="/order" className="nav-link active" aria-current="page">Order</Link>
                         </li>
                         
-                        <li className="nav-item">
-                            <Link className="nav-link" to="/register">Register</Link>
-                        </li>
+                       
+                        {AuthButtons}
                         
                     </ul>
                     
@@ -47,7 +84,7 @@ function NavbarFe() {
                 </div>
             </div>
         </nav>
-       
+
     )
 }
 
